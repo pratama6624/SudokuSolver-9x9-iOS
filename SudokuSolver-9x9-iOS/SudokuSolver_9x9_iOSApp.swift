49f4e12133576459e6a5d107f6ee505ff6dc3d63
pratama6624 <scyoung6624@gmail.com>
2024-10-17 21:14:14 +0700

    Initial Commit

---
+++ SudokuSolver-9x9-iOS/SudokuSolver_9x9_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuSolver_9x9_iOSApp.swift
+//  SudokuSolver-9x9-iOS
+//
+//  Created by Pratama One on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolver_9x9_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
